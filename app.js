1b42726b67de620b9f6cc7ef3b9e7bbc7b20d40e
jamie hansell <jamiehansell06@gmail.com>
2022-10-22 13:02:56 +0100

    i have created a function that writes to a file

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("hello world")
+
+const fs = require('fs')
+
+fs.appendFile('file.txt','writing to the file system! ',(error)=> {
+    console.log(error);
+});
+
+console.log('Data has been written to file.txt');
